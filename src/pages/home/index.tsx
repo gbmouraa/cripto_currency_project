3ca637fd849776c9a86d2e71ed0e681ed12f5b96
Gabriel Moura <gabrielmoura-cma@hotmail.com>
2025-03-06 23:46:21 -0300

    feat: add home

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,7 +1,60 @@
+import "./home-style.css";
+import { BsSearch } from "react-icons/bs";
+import { Link } from "react-router-dom";
+
 export const Home = () => {
   return (
-    <div>
-      <h1>Página Home</h1>
-    </div>
+    <main className="home-container">
+      <form className="home-form">
+        <input type="text" placeholder="Digite o nome da moeda" />
+        <button type="submit">
+          <BsSearch size={30} color="#fff" />
+        </button>
+      </form>
+      <table>
+        <thead>
+          <tr>
+            <th scope="col" className="home-th">
+              Moeda
+            </th>
+            <th scope="col" className="home-th">
+              Valor de Mercado
+            </th>
+            <th scope="col" className="home-th">
+              Preço
+            </th>
+            <th scope="col" className="home-th">
+              Volume
+            </th>
+            <th scope="col" className="home-th">
+              Mudança 24h
+            </th>
+          </tr>
+        </thead>
+        <tbody id="home-tbody">
+          <tr className="home-tr">
+            <td className="home-td-label" data-label="Moeda">
+              <div>
+                <Link to={"/detail/bitcoin"}>
+                  <span>Bitcoin</span> | BTC
+                </Link>
+              </div>
+            </td>
+            <td className="home-td-label" data-label="Valor de Mercado">
+              1T
+            </td>
+            <td className="home-td-label" data-label="Preço">
+              8.000
+            </td>
+            <td className="home-td-label" data-label="Volume">
+              2B
+            </td>
+            <td className="home-td-label" data-label="Mudança 24h">
+              <span>1.20</span>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </main>
   );
 };
